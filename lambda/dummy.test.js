b8eb2e6dda3c1e2c913f0d560204eb88dbe0df7c
Jason Darby <jasonkdarby@gmail.com>
2017-01-19 19:02:25 -0500

    re-reorganized, Mike was right all along
    took out express in favor of lambda

---
+++ lambda/dummy.test.js
@@ -0,0 +1,8 @@
+const lambdalocal = require('lambda-local');
+
+it('fakes lambda', () => {
+    lambdalocal.execute({
+        event: { "key1": "value1", "key2": "value2", "key3": "value3" },
+        lambdaPath: __dirname+'/index.js'
+    });
+});
