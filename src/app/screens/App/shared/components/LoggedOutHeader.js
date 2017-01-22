3929641eb022be134cf4de60c7a45a54237ed814
Michael Minutelli <mminutel@gmail.com>
2017-01-22 01:57:22 -0500

    Fixed an issue with the branch.

--- src/app/screens/App/shared/components/LoggedOutHeader.js
+++ src/app/screens/App/shared/components/LoggedOutHeader.js
@@ -3,32 +3,12 @@ import { Navbar, Nav, NavItem, Button, FormGroup, FormControl, InputGroup } from
 
 const LoggedOutHeader = () => (
     <Navbar>
-<<<<<<< HEAD
-        <Navbar.Header>
-            <Navbar.Brand>
-                <a href="#">Listopolis</a>
-            </Navbar.Brand>
-        </Navbar.Header>
-=======
->>>>>>> 1addcee673fd203fd3b60f3deeb827cd31c9f4d6
         <Nav>
             <NavItem disabled>Personal</NavItem>
             <NavItem disabled>Open source</NavItem>
             <NavItem disabled>Business</NavItem>
             <NavItem disabled>Explore</NavItem>
             <NavItem disabled>Help</NavItem>
-<<<<<<< HEAD
-            <NavItem disabled>Pricing</NavItem>
-            <NavItem disabled>Blog</NavItem>
-            <NavItem disabled>Support</NavItem>
-        </Nav>
-        <Nav pullRight>
-            <Navbar.Form>
-                <FormGroup>
-                    <FormControl type="text" placeholder="Search Listopolis"/>
-                </FormGroup>
-                {' '}
-=======
         </Nav>
         <Nav pullRight>
             <NavItem disabled>Pricing</NavItem>
@@ -43,12 +23,6 @@ const LoggedOutHeader = () => (
                     </FormGroup>
                 </Navbar.Form>
             </NavItem>
-            <NavItem>
->>>>>>> 1addcee673fd203fd3b60f3deeb827cd31c9f4d6
-                <Button>Sign in</Button>
-                {' '}
-                <Button>Sign up</Button>
-            </Navbar.Form>
         </Nav>
     </Navbar>
 );
