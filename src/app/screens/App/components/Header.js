ab6fabb02230417f3baca9aeffafd45d2821411c
Jason Darby <jasonkdarby@gmail.com>
2017-01-23 18:13:03 -0500

    refactored header

--- src/app/screens/App/components/Header.js
+++ src/app/screens/App/components/Header.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import { Navbar, Button, Grid, Row, Col } from 'react-bootstrap';
+import { Navbar, Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-//TODO: Fix for larger-than-mobile, buttons are obnoxiously large.
 const Header = () => (
     <Navbar>
         <Navbar.Header>
+            <Navbar.Brand>
+                <a href="/"><span className="glyphicon glyphicon-list-alt"/> Listopolis</a>
+            </Navbar.Brand>
             <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-            <Navbar.Form className="text-right">
-                <Grid fluid>
-                    <Row>
-                        <Col xs={6}>
-                            <LinkContainer to="/signup">
-                                <Button bsStyle="success" block>Sign up</Button>
-                            </LinkContainer>
-                        </Col>
-                        <Col xs={6}>
-                            <LinkContainer to="/login">
-                                <Button block>Sign in</Button>
-                            </LinkContainer>
-                        </Col>
-                    </Row>
-                </Grid>
+            <Navbar.Form className="hidden-xs" pullRight>
+                <ButtonToolbar>
+                    <ButtonGroup>
+                        <LinkContainer to="/signup">
+                            <Button bsStyle="success">Sign up</Button>
+                        </LinkContainer>
+                    </ButtonGroup>
+                    <ButtonGroup>
+                        <LinkContainer to="/login">
+                            <Button>Sign in</Button>
+                        </LinkContainer>
+                    </ButtonGroup>
+                </ButtonToolbar>
+            </Navbar.Form>
+            <Navbar.Form className="visible-xs">
+                <ButtonGroup justified>
+                    <LinkContainer to="/signup">
+                        <Button bsStyle="success" bsSize="large">Sign up</Button>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                        <Button bsSize="large">Sign in</Button>
+                    </LinkContainer>
+                </ButtonGroup>
             </Navbar.Form>
         </Navbar.Collapse>
     </Navbar>
