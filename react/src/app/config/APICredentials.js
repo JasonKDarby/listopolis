7b563534063887ea9e0ca5e1952bfec558d52cca
Jason Darby <jasonkdarby@gmail.com>
2017-02-15 23:14:00 -0500

    removed unused api credentials

--- react/src/app/config/APICredentials.js
+++ react/src/app/config/APICredentials.js
@@ -1,9 +1,5 @@
 //noinspection JSUnresolvedVariable
-const APICredentials = {
+export default {
     cognitoUserPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID,
-    cognitoAppClientId: process.env.REACT_APP_AWS_COGNITO_APP_CLIENT_ID,
-    awsCognitoIdentityPoolId: process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
-    googleLoginClientId: process.env.REACT_APP_GOOGLE_LOGIN_CLIENT_ID
+    cognitoAppClientId: process.env.REACT_APP_AWS_COGNITO_APP_CLIENT_ID
 }
-
-export default APICredentials
