cdf505cff2e88453b5a81c837566180a60855e7f
Jason Darby <jasonkdarby@gmail.com>
2017-02-27 23:19:43 -0500

    added a confirmation dialogue to the list deletion

--- react/src/app/screens/Welcome/screens/Login/screens/Lists/screens/List/components/Delete.js
+++ react/src/app/screens/Welcome/screens/Login/screens/Lists/screens/List/components/Delete.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Button, Glyphicon } from 'react-bootstrap';
+import { Button, Glyphicon, Modal, Row, Col } from 'react-bootstrap';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
-export default ({ onClick }) => (
-    <Button bsStyle="danger" bsSize="small" onClick={onClick}>
+const modalIsVisible = observable(false);
+
+const showModal = () => { modalIsVisible.set(true) };
+
+const hideModal = () => { modalIsVisible.set(false) };
+
+export default observer(({ onClick }) => (
+    <Button bsStyle="danger" bsSize="small" onClick={showModal}>
         <Glyphicon glyph="trash"/>
         {' '}
         Delete
+        <Modal show={modalIsVisible.get()} onHide={hideModal} bsSize="small" aria-labelledby="contained-modal-title-sm">
+            <Modal.Header>
+                <Modal.Title id="contained-modal-title-sm" className="text-center">
+                    Delete list confirmation
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>Are you sure you want to delete this list?</p>
+                <p>It'll be gone for good!</p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Row>
+                    <Col xs={12} className="text-center">
+                        <Button onClick={e => { hideModal(); onClick(e)}} bsStyle="danger">
+                            <strong>Yes</strong>, delete it.
+                        </Button>
+                        {' '}
+                        <Button onClick={hideModal}><strong>No</strong>, keep it.</Button>
+                    </Col>
+                </Row>
+            </Modal.Footer>
+        </Modal>
     </Button>
-);
+));
