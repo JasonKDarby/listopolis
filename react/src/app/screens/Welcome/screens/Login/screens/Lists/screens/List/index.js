13c3d665d2ecac9c40b844f0557296c88ae19d20
Jason Darby <jasonkdarby@gmail.com>
2017-02-27 21:44:59 -0500

    removed unnecessary grid column size parameter

--- react/src/app/screens/Welcome/screens/Login/screens/Lists/screens/List/index.js
+++ react/src/app/screens/Welcome/screens/Login/screens/Lists/screens/List/index.js
@@ -8,7 +8,7 @@ import { deleteList } from '../../../../../../../../shared/ListAPI';
 export default ({ params: { id }, user, history }) => (
     <div>
         <Row>
-            <Col xs={12} sm={8} smOffset={2}>
+            <Col sm={8} smOffset={2}>
                 <List id={id} user={user} />
             </Col>
         </Row>
