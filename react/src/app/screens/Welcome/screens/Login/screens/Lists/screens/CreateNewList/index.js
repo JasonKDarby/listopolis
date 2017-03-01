5cc6bd875408f6caa2960df856ecc963d1557533
Jason Darby <jasonkdarby@gmail.com>
2017-02-28 23:11:36 -0500

    split the mobx-react-form for creating new lists from the UI representation so I can reuse it on the edit list page
    I still need to factor out the UI elements to a component.

--- react/src/app/screens/Welcome/screens/Login/screens/Lists/screens/CreateNewList/index.js
+++ react/src/app/screens/Welcome/screens/Login/screens/Lists/screens/CreateNewList/index.js
@@ -2,100 +2,34 @@ import React from 'react';
 import { Form, FormGroup, FormControl, ControlLabel, HelpBlock, Button, ButtonGroup, Glyphicon, Col, Row, Alert } from 'react-bootstrap'
 import { observer } from 'mobx-react';
 import { hashHistory } from 'react-router'
-import MobxReactForm from 'mobx-react-form';
-import validatorjs from 'validatorjs';
 import './index.css';
 import autosize from 'autosize';
 import { createList } from '../../../../../../../../shared/ListAPI';
-
-const plugins = { dvr: validatorjs };
-
-const fields = [
-    'title',
-    'lines',
-    'lines[]'
-];
-
-const values = {
-    'lines': ['']
-};
-
-const labels = {
-    'title': 'Title',
-    'lines': 'New lines',
-    'lines[]': 'New line'
-};
-
-const placeholders = {
-    'title': 'Title',
-    'lines[]': 'New line'
-};
-
-const rules = {
-    'title': 'required|string|between:1,140',
-    'lines': 'required|array|between:1,10',
-    'lines[]': 'required|string|between:1,140'
-};
-
-class CreateNewListForm extends MobxReactForm {
-
-    errorMessages = [];
-
-    onSuccess(form) {
-        this.errorMessages = [];
-        let createRequestData = form.values();
-
-        createList(this.user.jwtToken, createRequestData, response => {
-            hashHistory.push(`/lists/${response.body.id}`);
-        });
-    }
-    onError(form) {
-        this.errorMessages = [];
-        if(form.errors().title) {
-            if(form.errors().title === "The Title field is required.") {
-                this.errorMessages.push(form.errors().title);//Don't do anything, this message is fine.
-            } else if(form.errors().title === "The Title field must be between 1 and 140.") {
-                this.errorMessages.push(`The Title must be between 1 and 140 characters.`);
-            } else {
-                this.errorMessages.push(`Error at title: ${form.errors().title}`);
-            }
-        }
-        form.errors().lines.forEach((lineError, index) => {
-            if(lineError) {
-                if(lineError === "The New line field is required.") {
-                    this.errorMessages.push(`Line ${index} can't be empty.`);
-                } else if(lineError === "The New line field must be between 1 and 140.") {
-                    this.errorMessages.push(`Line ${index} must be between 1 and 140 characters.`);
-                } else {
-                    this.errorMessages.push(`Error at line ${index}: ${lineError}`);
-                }
-            }
-        });
-        form.invalidate('There were errors with your submission.');
-    }
-}
-
-const createNewListForm = new CreateNewListForm({ fields, values, labels, placeholders, rules }, { plugins });
+import { createEmptyListForm } from './shared/ListForm';
 
 export default observer(class extends React.Component {
 
     componentWillMount() {
-        createNewListForm.user = this.props.user;
-        createNewListForm.reset();
+        this.createNewListForm = createEmptyListForm((requestData) => {
+            createList(this.props.user.jwtToken, requestData, response => {
+                hashHistory.push(`/lists/${response.body.id}`);
+            });
+        });
+        this.createNewListForm.reset();
     }
 
     render() {
         return (
-            <Form onSubmit={createNewListForm.onSubmit} horizontal>
+            <Form onSubmit={this.createNewListForm.onSubmit} horizontal>
                 <Row>
                     <Col xs={12} className="text-center">
-                        <p><strong>{createNewListForm.error}</strong></p>
+                        <p><strong>{this.createNewListForm.error}</strong></p>
                     </Col>
                 </Row>
                 <Row>
                     <Col sm={8} smOffset={2} className="text-center">
                         {
-                            createNewListForm.errorMessages.map(
+                            this.createNewListForm.errorMessages.map(
                                 (errorMessage, index) =>
                                     <Alert bsStyle="warning" key={index}>{errorMessage}</Alert>)
                         }
@@ -103,15 +37,15 @@ export default observer(class extends React.Component {
                 </Row>
                 <Row>
                     <Col sm={8} smOffset={2}>
-                        <FormGroup validationState={createNewListForm.$('title').isValid ? 'success' : 'error'}>
-                            <ControlLabel>{createNewListForm.$('title').label}</ControlLabel>
-                            <FormControl {...createNewListForm.$('title').bind()} />
+                        <FormGroup validationState={this.createNewListForm.$('title').isValid ? 'success' : 'error'}>
+                            <ControlLabel>{this.createNewListForm.$('title').label}</ControlLabel>
+                            <FormControl {...this.createNewListForm.$('title').bind()} />
                             <FormControl.Feedback />
                         </FormGroup>
                     </Col>
                 </Row>
                 <br/>
-                { createNewListForm.$('lines').map(
+                { this.createNewListForm.$('lines').map(
                     (line, index) =>
                     <Row key={line.key}>
                         <Col sm={10} smOffset={1}>
@@ -125,7 +59,7 @@ export default observer(class extends React.Component {
                                                 index > 0 &&
                                                     <Button type="button" onClick={() => {
                                                         let lines = [];
-                                                        createNewListForm.$('lines').fields
+                                                        this.createNewListForm.$('lines').fields
                                                             .forEach(field => lines.push(field.value));
                                                         let line = lines.splice(index-1, 1);
                                                         lines.splice(index, 0, line);
@@ -133,7 +67,7 @@ export default observer(class extends React.Component {
                                                         for(let i = 0; i < lines.length; ++i) {
                                                             flattened = flattened.concat(lines[i]);
                                                         }
-                                                        createNewListForm.set({
+                                                        this.createNewListForm.set({
                                                             lines: flattened
                                                         });
                                                     }}>
@@ -141,10 +75,10 @@ export default observer(class extends React.Component {
                                                     </Button>
                                             }
                                             {
-                                                index !== createNewListForm.$('lines').fields.size-1 &&
+                                                index !== this.createNewListForm.$('lines').fields.size-1 &&
                                                     <Button type="button" onClick={() => {
                                                         let lines = [];
-                                                        createNewListForm.$('lines').fields
+                                                        this.createNewListForm.$('lines').fields
                                                             .forEach(field => lines.push(field.value));
                                                         let line = lines.splice(index+1, 1);
                                                         lines.splice(index, 0, line);
@@ -152,7 +86,7 @@ export default observer(class extends React.Component {
                                                         for(let i = 0; i < lines.length; ++i) {
                                                             flattened = flattened.concat(lines[i]);
                                                         }
-                                                        createNewListForm.set({
+                                                        this.createNewListForm.set({
                                                             lines: flattened
                                                         });
                                                     }}>
@@ -177,7 +111,7 @@ export default observer(class extends React.Component {
                                             </Button>
                                             {
                                                 /*if there's only one field we don't want to be able to delete it*/
-                                                createNewListForm.$('lines').fields.size !== 1 &&
+                                                this.createNewListForm.$('lines').fields.size !== 1 &&
                                                 <Button type="button" onClick={line.onDel}>
                                                     <Glyphicon glyph="remove" />
                                                 </Button>
@@ -191,10 +125,10 @@ export default observer(class extends React.Component {
                                     <Col xs={10} xsOffset={1} className="text-center">
                                         <Button type="button" bsSize="xsmall" onClick={() => {
                                             let lines = [];
-                                            createNewListForm.$('lines').fields
+                                            this.createNewListForm.$('lines').fields
                                                 .forEach(field => lines.push(field.value));
                                             lines.splice(index+1, 0, '');
-                                            createNewListForm.update({
+                                            this.createNewListForm.update({
                                                 lines: lines
                                             });
                                         }}>
@@ -214,7 +148,7 @@ export default observer(class extends React.Component {
                                 type="submit"
                                 bsSize="large"
                                 bsStyle="success"
-                                onClick={createNewListForm.onSubmit}
+                                onClick={this.createNewListForm.onSubmit}
                             >
                                 Create new list
                             </Button>
